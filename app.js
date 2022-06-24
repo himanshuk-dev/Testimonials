5c2dd8eaa1a3965283e79cc0c844779d8d6751d6
Himanshu Kumar <himanshu.kumar2@outlook.com>
2022-06-24 11:59:23 -0700

    added app.js

--- app.js
+++ app.js
@@ -2,38 +2,31 @@
 const reviews = [
     {
       id: 1,
-      name: "susan smith",
-      job: "web developer",
-      img:
-        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
-      text:
-        "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+      name: "Susan A. Farmer",
+      job: "Software Developer",
+      img: src="Images/Susan.jpg",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     },
     {
-      id: 2,
-      name: "anna johnson",
-      job: "web designer",
-      img:
-        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
-      text:
-        "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
-    },
-    {
-      id: 3,
-      name: "peter jones",
-      job: "intern",
-      img:
-        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
-      text:
-        "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
-    },
-    {
-      id: 4,
-      name: "bill anderson",
-      job: "the boss",
-      img:
-        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
-      text:
-        "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
-    },
+        id: 2,
+        name: "Michael C. Haggerty",
+        job: "Business Analyst",
+        img: ,
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      },
+
+      {
+        id: 3,
+        name: "Carl C. Williams",
+        job: "Operations Manager",
+        img: ,
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dui vivamus arcu felis bibendum ut tristique et. Justo donec enim diam vulputate. Felis bibendum ut tristique et. Diam vel quam elementum pulvinar etiam non quam lacus suspendisse. Vitae elementum curabitur vitae nunc sed velit dignissim. Enim nunc faucibus a pellentesque sit amet porttitor eget. Quisque egestas diam in arcu cursus euismod. At lectus urna duis convallis. Nibh tellus molestie nunc non blandit massa enim nec dui. Scelerisque eu ultrices vitae auctor eu augue ut lectus arcu. Convallis posuere morbi leo urna molestie at elementum eu. Nec dui nunc mattis enim ut tellus elementum. Sapien nec sagittis aliquam malesuada bibendum arcu vitae elementum curabitur. Dui ut ornare lectus sit amet est placerat in. Arcu felis bibendum ut tristique et egestas quis ipsum. Nisi vitae suscipit tellus mauris a diam maecenas sed enim. Sit amet cursus sit amet dictum sit amet justo donec. Viverra maecenas accumsan lacus vel facilisis volutpat."
+      },
+      {
+        id: 4,
+        name: "Helen L. Jackson",
+        job: "Financial Advisor",
+        img:
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nulla at volutpat diam ut venenatis tellus in metus. Sed velit dignissim sodales ut. Proin fermentum leo vel orci porta non pulvinar neque laoreet. Sociis natoque penatibus et magnis dis. Ullamcorper sit amet risus nullam. Diam sollicitudin tempor id eu nisl nunc mi ipsum. Porta non pulvinar neque laoreet suspendisse. Phasellus vestibulum lorem sed risus ultricies. Congue mauris rhoncus aenean vel elit scelerisque. Blandit volutpat maecenas volutpat blandit. Mi ipsum faucibus vitae aliquet nec ullamcorper sit."
+      },
   ];
